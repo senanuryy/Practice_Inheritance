51d6abe873fea4bd96520f11926542befc344c76
Senanur Yavuzarslan <yavuzarslansenanur@gmail.com>
2025-01-16 18:05:07 +0300

    Add project files.

---
+++ Practice_Inheritance/BaseKisi.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Inheritance
+{
+    public class BaseKisi
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual void Introduce()
+        {
+            Console.WriteLine("FirstName: " + FirstName);
+            Console.WriteLine("LastName: " + LastName);
+        }
+    }
+
+    public class Student : BaseKisi
+    {
+        public string StudentNumber { get; set; }
+
+        public override void Introduce()
+        {
+            base.Introduce();
+            Console.WriteLine("Student Number: " + StudentNumber + "\n");
+        }
+    }
+
+    public class Teacher : BaseKisi
+    {
+        public int TeacherSalary { get; set; }
+
+        public override void Introduce()
+        {
+            base.Introduce();
+            Console.WriteLine("Teacher Salary: " + TeacherSalary + "\n");
+        }
+    }
+}
---
+++ Practice_Inheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿using Practice_Inheritance;
+
+Teacher teacher = new Teacher()
+{
+    FirstName = "Till",
+    LastName = "Lindemann",
+    TeacherSalary = 4444
+};
+
+Student student = new Student()
+{
+    FirstName = "Austin",
+    LastName = "Post",
+    StudentNumber = "1E2ST48"
+};
+
+teacher.Introduce();
+student.Introduce();
